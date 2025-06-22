6a51b6e34b95922447ed735ce2f50a395539bcd4
Pierre M. <promazelaygue@gmail.com>
2025-06-22 20:44:24 +0200

    Changement de la classe CSS des sections de la page d'accueil de "min-h-screen" à "h-fit" pour optimiser l'affichage et améliorer la présentation des composants Home, Skills, Projects, Timeline et Contact.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,19 +7,19 @@ import Projects from "../features/projects/index.jsx";
 const Home = () => {
   return (
     <>
-      <section id="home" className="min-h-screen">
+      <section id="home" className="h-fit">
         <Hero />
       </section>
-      <section id="skills" className="min-h-screen">
+      <section id="skills" className="h-fit">
         <Skills />
       </section>
-      <section id="projects" className="min-h-screen">
+      <section id="projects" className="h-fit">
         <Projects />
       </section>
-      <section id="timeline" className="min-h-screen">
+      <section id="timeline" className="h-fit">
         <Timeline />
       </section>
-      <section id="contact" className="min-h-screen">
+      <section id="contact" className="h-fit">
         <Contact />
       </section>
     </>
