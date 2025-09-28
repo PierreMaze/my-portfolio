ed392d25309b63add1ecc5ded7fe93f879ec414b
Pierre M. <promazelaygue@gmail.com>
2025-09-28 16:40:12 +0200

    Ajout de nouveaux composants de boutons : ButtonNavigationResponsiveUnderlineSecondaryHoveredUnderlinePrimaryFocusedAllPrimary et mise à jour de l'index des boutons pour inclure les exports des composants existants, améliorant ainsi la réutilisabilité et l'organisation des composants de l'interface utilisateur.

---
+++ src/components/ui/buttons/ButtonNavigationResponsiveUnderlineSecondaryHoveredUnderlinePrimaryFocusedAllPrimary.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ButtonNavigationResponsiveUnderlineSecondaryHoveredUnderlinePrimaryFocusedAllPrimary =
+  () => {
+    return <div></div>;
+  };
+
+export default ButtonNavigationResponsiveUnderlineSecondaryHoveredUnderlinePrimaryFocusedAllPrimary;
---
+++ src/components/ui/buttons/index.jsx
@@ -0,0 +1,6 @@
+// Export des composants de boutons
+export { ButtonIconsSecondaryHoveredColoredQuarteRotate } from "./ButtonIconsSecondaryHoveredColoredQuarteRotate";
+export { ButtonRectangularPrimary } from "./ButtonRectangularPrimary";
+export { ButtonRectangularSecondary } from "./ButtonRectangularSecondary";
+export { ButtonRectangularSecondaryHoveredSecondaryFocusedPrimary } from "./ButtonRectangularSecondaryHoveredSecondaryFocusedPrimary";
+export { ButtonUnderlineSecondaryHoveredPrimary } from "./ButtonUnderlineSecondaryHoveredPrimary";
