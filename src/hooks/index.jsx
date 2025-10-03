e9a2376d2c48b0e8d12840a5a50d57dff7a38902
Pierre M. <promazelaygue@gmail.com>
2025-10-03 11:52:18 +0200

    Suppression du hook personnalisé useProjectsSelector et mise à jour des exports dans le fichier d'index des hooks. Ce changement simplifie la gestion des projets en éliminant un composant inutilisé.

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -2,6 +2,5 @@
 export { default as useMeta } from "./useMeta";
 export { default as useProjectCategoryFilter } from "./useProjectCategoryFilter";
 export { default as useProjectsData } from "./useProjectsData";
-export { default as useProjectsSelector } from "./useProjectsSelector";
 export { default as useScrollToTop } from "./useScrollToTop";
 export { default as useSmoothScroll } from "./useSmoothScroll";
