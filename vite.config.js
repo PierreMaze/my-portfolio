67c585992668a983df23f9880e2bb24bc7c057ed
PierreMaze Desktop <promazelaygue@gmail.com>
2025-12-16 00:18:15 +0100

    Update Version TailwindCSS with ViteJS

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+  base: "/",
   server: {
     port: 3000,
     open: true,
-  },
-  build: {
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          // Garder les images avec un nom plus prévisible
-          if (
-            assetInfo.name &&
-            /\.(png|jpe?g|gif|svg|webp)$/.test(assetInfo.name)
-          ) {
-            return "assets/[name].[hash][extname]";
-          }
-          return "assets/[name].[hash][extname]";
-        },
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          router: ["react-router-dom"],
-          icons: ["react-icons"],
-        },
-      },
-    },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
   },
 });
